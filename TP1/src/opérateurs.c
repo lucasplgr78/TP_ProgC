98ccd062f267d2a346ca34c26b267fe965a4a75c
lucasplgr78 <lucas.pellegrin@edu.esiee-it.fr>
2025-11-21 15:07:43 +0100

    Update opérateurs.c

---
+++ TP1/src/opérateurs.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int a = 16;
+    int b = 3;
+
+    // Opérations arithmétiques
+    int addition = a + b;
+    int soustraction = a - b;
+    int multiplication = a * b;
+    int division = a / b;       // division entière
+    int modulo = a % b;
+
+    // Opérations logiques (comparaisons)
+    int egal = (a == b);
+    int superieur = (a > b);
+
+    // Affichage
+    printf("a = %d\n", a);
+    printf("b = %d\n\n", b);
+
+    printf("Addition : a + b = %d\n", addition);
+    printf("Soustraction : a - b = %d\n", soustraction);
+    printf("Multiplication : a * b = %d\n", multiplication);
+    printf("Division : a / b = %d\n", division);
+    printf("Modulo : a %% b = %d\n\n", modulo);
+
+    printf("a == b ? %d\n", egal);
+    printf("a > b ? %d\n", superieur);
+
+    return 0;
+}
