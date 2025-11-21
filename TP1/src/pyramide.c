7b93959a3c1c1e3a3e401b9eeaa9dedbeac1cac8
lucasplgr78 <lucas.pellegrin@edu.esiee-it.fr>
2025-11-21 15:27:54 +0100

    Update pyramide.c

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int n = 5; // hauteur de la pyramide
+    int i, j;
+
+    for(i = 1; i <= n; i++) {
+        // afficher les espaces pour centrer
+        for(j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+
+        // afficher les nombres croissants
+        for(j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+
+        // afficher les nombres décroissants
+        for(j = i - 1; j >= 1; j--) {
+            printf("%d", j);
+        }
+
+        printf("\n"); // fin de ligne
+    }
+
+    printf("\nPyramide générée !\n");
+
+    return 0;
+}
 
