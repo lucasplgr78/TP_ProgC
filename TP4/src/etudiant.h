d08e131bfa8811785beb3e6fe7b7c18b5a19f055
lucasplgr78 <lucas.pellegrin@edu.esiee-it.fr>
2025-12-01 11:06:11 +0100

    Update etudiant.h

--- TP4/src/etudiant.h
+++ TP4/src/etudiant.h
@@ -1,1 +1,17 @@
+#ifndef ETUDIANT_H
+#define ETUDIANT_H
+
+#define TAILLE_NOM 50
+#define TAILLE_PRENOM 50
+#define TAILLE_ADRESSE 100
+
+typedef struct {
+    char nom[TAILLE_NOM];
+    char prenom[TAILLE_PRENOM];
+    char adresse[TAILLE_ADRESSE];
+    float note1;
+    float note2;
+} Etudiant;
+
+#endif
 
