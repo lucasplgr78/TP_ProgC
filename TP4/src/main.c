716d7d1c105847f8f07d9ed6240a798389e28fa7
lucasplgr78 <lucas.pellegrin@edu.esiee-it.fr>
2025-12-01 10:42:26 +0100

    Update main.c

--- TP4/src/main.c
+++ TP4/src/main.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
+#include "operator.h"
+
+int main() {
+    int num1, num2;
+    char op;
+
+    printf("Entrez num1 : ");
+    scanf("%d", &num1);
+    printf("Entrez num2 : ");
+    scanf("%d", &num2);
+    printf("Entrez l'opérateur (+, -, *, /, %%, &, |, ~) : ");
+    scanf(" %c", &op); // espace avant %c pour ignorer le retour à la ligne précédent
+
+    int result;
+
+    switch(op) {
+        case '+': result = somme(num1, num2); break;
+        case '-': result = difference(num1, num2); break;
+        case '*': result = produit(num1, num2); break;
+        case '/': result = quotient(num1, num2); break;
+        case '%': result = modulo(num1, num2); break;
+        case '&': result = et(num1, num2); break;
+        case '|': result = ou(num1, num2); break;
+        case '~': result = negation(num1); break;
+        default:
+            printf("Opérateur inconnu.\n");
+            return 1;
+    }
+
+    printf("Résultat : %d\n", result);
+    return 0;
+}
 
